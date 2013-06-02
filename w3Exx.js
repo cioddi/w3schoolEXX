a1547925fe0c18ac4b0a81ee0952a081730f6782
Gustavo Rodrigues <github@blio.tk>
2013-06-02 13:04:33 -0300

    Removed jQuery, added more Google domains

--- w3Exx.js
+++ w3Exx.js
@@ -1,16 +1,43 @@
-function removeW3Schools(){
-	$('li.g').each(function(idx,item){
-		if($(item).find('cite').html().indexOf('w3schools.com/') !== -1){
-
-			$(item).remove();
-
+// Why jQuery?
+function inject(){
+    var searchEl,
+	removeW3Schools = function () {
+        searchEl = document.getElementById('search');
+    
+        // But it is weird:
+		var iterator = document.evaluate('//OL[@id="rso"]/li/div/h3/a[contains(@href,"w3schools")]/../../..', searchEl, null, XPathResult.ANY_TYPE, null)
+ 
+		try {
+			var thisNode = iterator.iterateNext();
+			
+			while (thisNode) {
+				thisNode.parentNode.removeChild(thisNode);
+				thisNode = iterator.iterateNext();
+			} 
 		}
-	});
+		catch (e) {
+			// Error: Document tree modified during iteration'
+            setTimeout(removeW3Schools, 1500);
+		}
+        searchEl.removeEventListener('DOMNodeInserted');
+        searchEl.addEventListener('DOMNodeInserted', throttle, false);
+        searchEl.addEventListener('DOMNodeRemoved', throttle, false);
+	},
+    
+    throttleTimer = null,
+    throttle = function () {
+        if ( throttleTimer ) {
+            clearTimeout(throttleTimer);
+        }
+        throttleTimer = setTimeout(removeW3Schools, 500);
+    };
+    
+    // Init
+    window.addEventListener('load', throttle, false);
+    throttle();
 }
 
-setTimeout(removeW3Schools,500);
+var s = document.createElement('script');
+s.innerHTML = '~' + inject.toString() + '()';
+(document.head||document.documentElement).appendChild(s);
 
-$('[name="btnG"]').click(function(){setTimeout(removeW3Schools,500);});
-$('[name="q"]').focusout(function(){
-	setTimeout(removeW3Schools,500);
-});
