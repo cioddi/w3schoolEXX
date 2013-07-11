dacd8b26ed6d6ec4c3ea0ceb8716fa8e8deef620
Gustavo Rodrigues <github@blio.tk>
2013-07-11 12:15:43 -0300

    Simplied script, fixed some errors
    It now works when you change pages on Google (I don't know why it was
    not working before) and don't inject scripts.

--- w3Exx.js
+++ w3Exx.js
@@ -1,54 +1,43 @@
-function inject(){
-    var searchEl,
-	removeW3Schools = function () {
-        // Search only on #search, for faster results:
-        searchEl = document.getElementById('search');
-        if (!searchEl) {
-            searchEl = document.body;
-        }
-    
-        // Why jQuery?
-		var iterator = document.evaluate('//OL[@id="rso"]/li/div/h3/a[contains(@href,"w3schools")]/../../..', searchEl, null, XPathResult.ANY_TYPE, null)
- 
-		try {
-			var thisNode = iterator.iterateNext();
-			
-			while (thisNode) {
-				thisNode.parentNode.removeChild(thisNode);
-				thisNode = iterator.iterateNext();
-			} 
-		}
-		catch (e) {
-			// Error: Document tree modified during iteration'
-            setTimeout(removeW3Schools, 1500);
-		}
-        
-        // Run the funcion again when the DOM changes:
-        searchEl.removeEventListener('DOMNodeInserted');
-        searchEl.addEventListener('DOMNodeInserted', throttle, false);
-        searchEl.addEventListener('DOMNodeRemoved', throttle, false);
-	},
-    
-    throttleTimer = null,
-    throttle = function () {
-        if ( throttleTimer ) {
-            clearTimeout(throttleTimer);
-        }
-        throttleTimer = setTimeout(removeW3Schools, 500);
-    };
-    
-    // Init on the load event and on script injection:
-    window.addEventListener('load', throttle, false);
-    throttle();
-}
+var searchEl,
+  performRemove = function () {
+    if (searchEl) {
+      searchEl.removeEventListener('DOMNodeInserted');
+    }
 
+    // Search only on #search, for faster results:
+    searchEl = document.getElementById('search');
+    if (!searchEl) {
+      // If #search not found, search on body:
+      searchEl = document.body;
+    }
 
-/* Inject the script:
- I personally prefer this way to gain access to a greater
- number of javascript functions
- but it can easily disabled, if you prefer. */
+    // TODO: improve it to be more future-proof and faster:
+    var iterator = document.evaluate('//OL[@id="rso"]/li/div/h3/a[contains(@href,"w3schools")]/../../..', searchEl, null, XPathResult.ANY_TYPE, null)
 
-var s = document.createElement('script');
-s.innerHTML = '~' + inject.toString() + '()';
-(document.head||document.documentElement).appendChild(s);
+    try {
+      var thisNode = iterator.iterateNext();
 
+      while (thisNode) {
+        thisNode.parentNode.removeChild(thisNode);
+        thisNode = iterator.iterateNext();
+      }
+    } catch (e) {
+      // Error: Document tree modified during iteration'
+      setTimeout(performRemove, 1500);
+    }
+
+    // Run the funcion again when the DOM changes:
+    searchEl.addEventListener('DOMNodeInserted', throttle, false);
+    searchEl.addEventListener('DOMNodeRemoved', throttle, false);
+  },
+
+  throttleTimer = null,
+  throttle = function () {
+    if (throttleTimer) {
+      clearTimeout(throttleTimer);
+    }
+    throttleTimer = setTimeout(performRemove, 500);
+  };
+
+// Init on the load event and on script injection:
+window.addEventListener('load', throttle, false); throttle();
